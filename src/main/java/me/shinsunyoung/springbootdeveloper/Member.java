14a867aabe11df4055b5af39433e63fb0b1d9f85
adorahelen <adorahelenmin@gmail.com>
2024-03-16 20:25:48 +0900

    24_03_16

--- src/main/java/me/shinsunyoung/springbootdeveloper/Member.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/Member.java
@@ -10,11 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Entity
+
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String name;
 }
